5313cd3b658cef3eb2c74129584641322976b465
Abdulkadirliban <abdulkadirliban0556@gmail.com>
2023-01-15 06:54:33 -0800

    ecomerce-flutter

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:electronic_ecomerce/screens/home.dart';
+import 'package:flutter/material.dart';
+
+void main()=> runApp(Electronic());
+
+class Electronic extends StatelessWidget {
+  const Electronic({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return HomeScreen();
+  }
+}
---
+++ lib/screens/home.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text("Elecronics"),
+          ),
+        ),
+      ),
+    );
+  }
+}
